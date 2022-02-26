2dacb6301d97ba656f7a56ab32dab98dfc2037ca
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-02-27 03:58:20 +0800

    19

--- pages/cart.js
+++ pages/cart.js
@@ -23,8 +23,10 @@ import {
 } from '@material-ui/core';
 import client from '../graphql/apollo-client';
 import { checkStockOneProduct } from '../graphql/schema/product/check-stock-one-product';
+import { useRouter } from 'next/router';
 
 function CartScreen() {
+    const router = useRouter();
     const { state, dispatch } = useContext(Store);
     const {
         cart: { cartItems },
@@ -45,6 +47,9 @@ function CartScreen() {
     };
     const removeItemHandler = (item) => {
         dispatch({ type: 'CART_REMOVE_ITEM', payload: item });
+    };
+    const checkoutHandler = () => {
+        router.push('/shipping');
     };
     return (
         <Layout title="Shopping Cart">
@@ -136,7 +141,12 @@ function CartScreen() {
                                     </Typography>
                                 </ListItem>
                                 <ListItem>
-                                    <Button variant="contained" color="primary" fullWidth>
+                                    <Button
+                                        onClick={checkoutHandler}
+                                        variant="contained"
+                                        color="primary"
+                                        fullWidth
+                                    >
                                         Check Out
                                     </Button>
                                 </ListItem>
