ca16541db0ac20eb9be45c59b264978e12ecda4b
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-02-27 03:16:39 +0800

    s

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import NextLink from 'next/link';
 import Image from 'next/image';
 import {
@@ -16,8 +16,10 @@ import Layout from '../../components/Layout';
 import useStyles from '../../utils/styles';
 import client from '../../graphql/apollo-client';
 import { getOneProduct } from '../../graphql/schema/product/get-one-product';
+import { Store } from '../../utils/Store';
 
 export default function ProductScreen(props) {
+    const { dispatch } = useContext(Store);
     const { product } = props;
     const classes = useStyles();
     // const router = useRouter();
@@ -26,6 +28,15 @@ export default function ProductScreen(props) {
     if (!product) {
         return <div>Product Not Found</div>;
     }
+    const addToCartHandler = async () => {
+        // const { data } = await axios.get(`/api/products/${product._id}`);
+        // if (data.countInStock <= 0) {
+        //     window.alert('Sorry. Product is out of stock');
+        //     return;
+        // }
+        // dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } });
+    };
+
     return (
         <Layout title={product.name} description={product.description}>
             <div className={classes.section}>
@@ -94,7 +105,12 @@ export default function ProductScreen(props) {
                                 </Grid>
                             </ListItem>
                             <ListItem>
-                                <Button fullWidth variant="contained" color="primary">
+                                <Button
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={addToCartHandler}
+                                >
                                     Add to cart
                                 </Button>
                             </ListItem>
