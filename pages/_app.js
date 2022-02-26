09e67b2ab911f7bdd3feb2af911db1edb757c495
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-02-27 01:47:20 +0800

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,7 @@
-import { SnackbarProvider } from 'notistack';
-import { useEffect } from 'react'
 import '../styles/globals.css'
-import { StoreProvider } from '../utils/Store';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-  return (
-    <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
-      <StoreProvider>
-        <Component {...pageProps} />
-      </StoreProvider>
-    </SnackbarProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
