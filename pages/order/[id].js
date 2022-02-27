1ad9183ebc4f1059346b6e9b101970a37f88a34a
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-02-27 23:33:35 +0800

    30

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useReducer, useState } from 'react';
+import React, { useContext, useEffect, useReducer } from 'react';
 import dynamic from 'next/dynamic';
 import Layout from '../../components/Layout';
 import { Store } from '../../utils/Store';
