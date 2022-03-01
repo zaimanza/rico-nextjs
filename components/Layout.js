f0da9b8b46f814b0fe02ffad624318e7199ebc32
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-03-01 19:41:39 +0800

    i

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import Head from 'next/head';
 import NextLink from 'next/link';
 import {
@@ -15,10 +15,27 @@ import {
     Menu,
     MenuItem,
     createTheme,
+    Box,
+    IconButton,
+    Drawer,
+    List,
+    ListItem,
+    Divider,
+    ListItemText,
+    InputBase,
 } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import CancelIcon from '@material-ui/icons/Cancel';
+import SearchIcon from '@material-ui/icons/Search';
+import { getManyProductCategory } from '../graphql/schema/product/get-many-product-category';
 import useStyles from '../utils/styles';
 import { Store } from '../utils/Store';
+
+import { useState } from 'react';
 import { useRouter } from 'next/router';
+import { useSnackbar } from 'notistack';
+import { useEffect } from 'react';
+import client from '../graphql/apollo-client';
 
 export default function Layout({ title, description, children }) {
     const router = useRouter();
@@ -48,6 +65,43 @@ export default function Layout({ title, description, children }) {
         },
     });
     const classes = useStyles();
+
+    const [sidbarVisible, setSidebarVisible] = useState(false);
+    const sidebarOpenHandler = () => {
+        setSidebarVisible(true);
+    };
+    const sidebarCloseHandler = () => {
+        setSidebarVisible(false);
+    };
+
+    const [categories, setCategories] = useState([]);
+    const { enqueueSnackbar } = useSnackbar();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const fetchCategories = async () => {
+        try {
+            const { data } = await client.query({
+                query: getManyProductCategory,
+            });
+            setCategories(data.getManyProductCategory);
+        } catch (err) {
+            enqueueSnackbar("There's an error", { variant: 'error' });
+        }
+    };
+
+    const [query, setQuery] = useState('');
+    const queryChangeHandler = (e) => {
+        setQuery(e.target.value);
+    };
+    const submitHandler = (e) => {
+        e.preventDefault();
+        router.push(`/search?query=${query}`);
+    };
+
+    useEffect(() => {
+        fetchCategories();
+    }, [fetchCategories]);
+
     const darkModeChangeHandler = () => {
         dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
         const newDarkMode = !darkMode;
@@ -69,7 +123,8 @@ export default function Layout({ title, description, children }) {
         localStorage.removeItem('userInfo');
         localStorage.removeItem('cartItems');
         localStorage.removeItem("token");
-
+        localStorage.removeItem('shippinhAddress');
+        localStorage.removeItem('paymentMethod');
         router.push('/');
     };
     return (
@@ -81,13 +136,79 @@ export default function Layout({ title, description, children }) {
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <AppBar position="static" className={classes.navbar}>
-                    <Toolbar>
-                        <NextLink href="/" passHref>
-                            <Link>
-                                <Typography className={classes.brand}>amazona</Typography>
-                            </Link>
-                        </NextLink>
-                        <div className={classes.grow}></div>
+                    <Toolbar className={classes.toolbar}>
+                        <Box display="flex" alignItems="center">
+                            <IconButton
+                                edge="start"
+                                aria-label="open drawer"
+                                onClick={sidebarOpenHandler}
+                                className={classes.menuButton}
+                            >
+                                <MenuIcon className={classes.navbarButton} />
+                            </IconButton>
+                            <NextLink href="/" passHref>
+                                <Link>
+                                    <Typography className={classes.brand}>amazona</Typography>
+                                </Link>
+                            </NextLink>
+                        </Box>
+                        <Drawer
+                            anchor="left"
+                            open={sidbarVisible}
+                            onClose={sidebarCloseHandler}
+                        >
+                            <List>
+                                <ListItem>
+                                    <Box
+                                        display="flex"
+                                        alignItems="center"
+                                        justifyContent="space-between"
+                                    >
+                                        <Typography>Shopping by category</Typography>
+                                        <IconButton
+                                            aria-label="close"
+                                            onClick={sidebarCloseHandler}
+                                        >
+                                            <CancelIcon />
+                                        </IconButton>
+                                    </Box>
+                                </ListItem>
+                                <Divider light />
+                                {categories.map((category) => (
+                                    <NextLink
+                                        key={category}
+                                        href={`/search?category=${category}`}
+                                        passHref
+                                    >
+                                        <ListItem
+                                            button
+                                            component="a"
+                                            onClick={sidebarCloseHandler}
+                                        >
+                                            <ListItemText primary={category}></ListItemText>
+                                        </ListItem>
+                                    </NextLink>
+                                ))}
+                            </List>
+                        </Drawer>
+
+                        <div className={classes.searchSection}>
+                            <form onSubmit={submitHandler} className={classes.searchForm}>
+                                <InputBase
+                                    name="query"
+                                    className={classes.searchInput}
+                                    placeholder="Search products"
+                                    onChange={queryChangeHandler}
+                                />
+                                <IconButton
+                                    type="submit"
+                                    className={classes.iconButton}
+                                    aria-label="search"
+                                >
+                                    <SearchIcon />
+                                </IconButton>
+                            </form>
+                        </div>
                         <div>
                             <Switch
                                 checked={darkMode}
@@ -95,16 +216,18 @@ export default function Layout({ title, description, children }) {
                             ></Switch>
                             <NextLink href="/cart" passHref>
                                 <Link>
-                                    {cart.cartItems.length > 0 ? (
-                                        <Badge
-                                            color="secondary"
-                                            badgeContent={cart.cartItems.length}
-                                        >
-                                            Cart
-                                        </Badge>
-                                    ) : (
-                                        'Cart'
-                                    )}
+                                    <Typography component="span">
+                                        {cart.cartItems.length > 0 ? (
+                                            <Badge
+                                                color="secondary"
+                                                badgeContent={cart.cartItems.length}
+                                            >
+                                                Cart
+                                            </Badge>
+                                        ) : (
+                                            'Cart'
+                                        )}
+                                    </Typography>
                                 </Link>
                             </NextLink>
                             {userInfo ? (
@@ -150,7 +273,9 @@ export default function Layout({ title, description, children }) {
                                 </>
                             ) : (
                                 <NextLink href="/login" passHref>
-                                    <Link>Login</Link>
+                                    <Link>
+                                        <Typography component="span">Login</Typography>
+                                    </Link>
                                 </NextLink>
                             )}
                         </div>
---
+++ graphql/schema/product/get-many-product-category.js
@@ -0,0 +1,7 @@
+import { gql } from "@apollo/client";
+
+export const getManyProductCategory = gql`
+query Query {
+  getManyProductCategory
+}
+`
